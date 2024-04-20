d80cdfa494f7ab3db6b0d358b287466db47924d4
Yulia Volkova <volkova.yulia.leonidovna@gmail.com>
2024-04-20 16:18:54 +0200

    Cosmic_oncoKB_annotation

---
+++ Code/Annotation/COSMIC_oncoKB/Add_known_oncogenes_TSG_COSMIC_oncoKB.R
@@ -0,0 +1,43 @@
+library(tidyr)
+library(dplyr)
+library(openxlsx)
+
+setwd() #here write you information
+
+# IMPORT DATASETS (keep information about oncogenes and TSG)
+
+data <- read.csv("./result_files/Table_with_gene_names_from_gencode.csv")
+COSMIC <- read.csv("Cancer_Gene_census_COSMIC.tsv", sep = "\t")
+oncoKB <- read.csv("cancerGeneList_OncoKB.tsv", sep = "\t")
+
+# KEEP ONLY RELATED COLUMNS
+
+COSMIC <- COSMIC %>%
+  select("Gene.Symbol", "Tier", "Hallmark", "Role.in.Cancer", "Mutation.Types", 
+         "Translocation.Partner")
+colnames(COSMIC) <- paste(colnames(COSMIC), ".COSMIC", sep = "")
+COSMIC <- COSMIC[grepl("TSG|oncogene", COSMIC$Role.in.Cancer), ]
+
+
+oncoKB <- oncoKB %>%
+  select("Hugo.Symbol", "Is.Oncogene", "Is.Tumor.Suppressor.Gene", "OncoKB.Annotated")
+colnames(oncoKB) <- paste(colnames(oncoKB), ".oncoKB", sep = "")
+oncoKB <- oncoKB[(oncoKB$Is.Tumor.Suppressor.Gene.oncoKB=="Yes"|oncoKB$Is.Oncogene=="Yes"), ]
+
+# MERGE TABLES INTO OUR DATA
+
+data_with_COSMIC <- left_join(data, COSMIC, join_by("gene_name" == "Gene.Symbol.COSMIC"))
+data_with_COSMIC_oncoKB <- left_join(data_with_COSMIC, oncoKB, join_by("gene_name" == "Hugo.Symbol.oncoKB"))
+
+data_with_COSMIC_oncoKB$Tier.COSMIC <- as.character(data_with_COSMIC_oncoKB$Tier.COSMIC)
+colnames_to_replace_na <- c("Tier.COSMIC", "Hallmark.COSMIC", 
+                            "Role.in.Cancer.COSMIC", "Mutation.Types.COSMIC",
+                            "Translocation.Partner.COSMIC", "Is.Oncogene.oncoKB",
+                            "Is.Tumor.Suppressor.Gene.oncoKB", "OncoKB.Annotated.oncoKB")   
+data_with_COSMIC_oncoKB <- data_with_COSMIC_oncoKB %>%
+  mutate_at(colnames_to_replace_na, ~replace_na(.x, "No_data"))
+
+# EXPORT TABLE
+
+write.csv(data_with_COSMIC_oncoKB, file = "Table_COSMIC_oncoKB_oncogenes_TSG.csv", row.names = FALSE)
+
